7f1669a24f47f83e44bb7c9c26813c18d6b0fa90
OpaogunProsper <prblvck@gmail.com>
2024-10-10 20:22:01 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    < div className={styles.App}>
-   Hello there
+    <div className={styles.App}>
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
