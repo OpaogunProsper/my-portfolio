223b8e340b1d67a0cf6282c4c9c75001be0aa0f1
OpaogunProsper <prblvck@gmail.com>
2024-10-11 11:41:20 +0100

    Contactcleanup

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -11,18 +11,22 @@ const Contact = () => {
        <ul className={styles.links}>
          <li className={styles.link}>
            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-           <a href="mailto:myemail@email.com">myemail@email.com</a>
+           <a href="mailto:prosperopaogun@gmail.com">
+             prosperopaogun@gmail.com
+           </a>
          </li>
          <li className={styles.link}>
            <img
              src={getImageUrl("contact/linkedinIcon.png")}
              alt="LinkedIn icon"
            />
-           <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+           <a href="https://www.linkedin.com/in/prosperopaogun/">
+             linkedin.com/in/prosperopaogun/
+           </a>
          </li>
          <li className={styles.link}>
            <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-           <a href="https://www.github.com/myname">github.com/myname</a>
+           <a href="https://www.github.com/OpaogunProsper">github.com/OpaogunProsper</a>
          </li>
        </ul>
      </footer>
